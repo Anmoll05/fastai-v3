613fe5183cbba60c60321ebf8eb43bd20ed30a5e
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2019-06-07 18:22:56 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,16 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/open?id=1rlwe5xETl_Z9TbRKREEkrguCoyj1VeXa'
+export_file_name = 'model_pickle.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Melanocytic nevi',
+     'Melanoma',
+     'Benign keratosis ',
+     'Basal cell carcinoma',
+     'Actinic keratoses',
+    'Vascular lesions',
+     'Dermatofibroma']
 path = Path(__file__).parent
 
 app = Starlette()
