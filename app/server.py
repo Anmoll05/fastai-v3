316e208553905517b2faa2aafa63f2c71b27bd3a
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2019-06-10 11:32:33 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1rlwe5xETl_Z9TbRKREEkrguCoyj1VeXa'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1rlwe5xETl_Z9TbRKREEkrguCoyj1VeXa'
 export_file_name = 'model_pickle.pkl'
 
 classes = ['Melanocytic nevi',
